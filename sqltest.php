76f0bf4a4827b1dee74565e97fc0ddca8e7f0f8e
bokwoon <bokwoon.c@gmail.com>
2018-10-19 00:08:40 +0800

    added sqltest.php

---
+++ sqltest.php
@@ -0,0 +1,29 @@
+<?php // sqltest.php
+require_once 'login.php';
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die("Fatal Error");
+
+if (isset($_POST['delete']) && isset($_POST['isbn'])) {
+  $isbn   = get_post($conn, 'isbn');
+  $query  = "DELETE FROM classics where isbn='$isbn'";
+  $result = $conn->query($query);
+  if (!$result) echo "DELETE failed<br><br>";
+}
+
+if (
+  isset($_POST['author'])
+  && isset($_POST['title'])
+  && isset($_POST['category'])
+  && isset($_POST['year'])
+  && isset($_POST['isbn'])
+) {
+  $author = get_post($conn, 'author');
+  $title = get_post($conn, 'title');
+  $category = get_post($conn, 'category');
+  $year = get_post($conn, 'year');
+  $isbn = get_post($conn, 'isbn');
+  $query = "INSERT INTO classics VALUES" . "('$author','$title','$category','$year','$isbn')";
+  $result = $conn->query($query);,
+  if (!$result) echo "INSERT failed<br><br>";
+}
+?>
