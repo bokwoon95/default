e992fa068291f9f9137e3adc925ca73c0e136583
bokwoon <bokwoon.c@gmail.com>
2018-11-26 11:52:49 +0800

    Added new functions to helpers.php
    - getColumnInfo()
    - The rewritten version of displayTable()
    - The rewritten version of nonDuplicateInsert()
    Removed the functions accordingly from cats.php

--- cats.php
+++ cats.php
@@ -3,48 +3,9 @@
 # https://stackoverflow.com/questions/19347005/how-can-i-explode-and-trim-whitespace#19347006
 # preg_split('/\s*(\s*,\s*)*,+\s*(\s*,\s*)*/i', $input_string);
 require_once 'login.php';
+require_once 'helpers.php';
 $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die("Fatal Error");
-
-function displayTable() {
-  global $conn;
-  $query = "SELECT * FROM cats";
-  if (!$result = $conn->query($query)) die("failed to select from 'cats' table<br>");
-  $rows = $result->num_rows;
-  echo "<br><table><tr><th>Id</th><th>Family</th><th>Name</th><th>Age</th></tr>";
-  for ($j=0; $j<$rows; $j++) {
-    $row = $result->fetch_array(MYSQLI_NUM);
-    echo "<tr>";
-    for ($k=0; $k<4; $k++) {
-      echo "<td>" . htmlspecialchars($row[$k]) . "</td>";
-    }
-    echo "</tr>";
-  }
-  echo "</table>";
-}
-
-function nonDuplicateInsert($insertcmd) {
-  global $conn;
-  preg_match('/insert into (\w+) values\((.+)\)/i', $insertcmd, $arr);
-  if (count($arr) != 3) { $errmsg="regex match failure for \$insertcmd"; return $errmsg; }
-  $table = $arr[1];
-  $args_str = $arr[2];
-  $values_array = preg_split('/\s*(\s*,\s*)*,+\s*(\s*,\s*)*/i', $args_str);
-  if (count($values_array) != 4) { $errmsg="wrong number of values in \$insertcmd"; return $errmsg; }
-  $id     = trim($values_array[0]);
-  $family = trim($values_array[1]);
-  $name   = trim($values_array[2]);
-  $age    = trim($values_array[3]);
-  $query_sel = "SELECT * FROM $table where family=$family and name=$name and age=$age";
-  if (!$result_sel = $conn->query($query_sel)) die("'$query_sel' failed :DDD");
-  if ($result_sel->num_rows == 0) {
-    if (!$result_ins = $conn->query($insertcmd)) die("'$insert_cmd' failed :DDD");
-    return "entry inserted";
-  } else {
-    return "entry already exists";
-  }
-  return "nonDuplicateInsert_end";
-}
 
 # create 'cats' table if it doesn't exist
 if ($result = $conn->query("SHOW TABLES LIKE 'cats'")) {
@@ -88,7 +49,9 @@ $result = nonDuplicateInsert("INSERT INTO cats VALUES(NULL, 'Cougar', 'Growler',
 $result = nonDuplicateInsert("INSERT INTO cats VALUES(NULL, 'Cheetah', 'Charly', 4)");
 
 # Displaying data from 'cats' table
-displayTable();
+if (!$result=$conn->query("SELECT * FROM cats")) die("nohomo");
+s();
+displayTable($result, "cats");
 
 # Renaming the cheetah 'Charly' to 'Charlie'
 # Warning! This has the effect of creating duplicate "Charlie" entries
--- helpers.php
+++ helpers.php
@@ -1,4 +1,8 @@
 <?php
+require_once 'login.php';
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die("Fatal Error");
+
 function test($bool,$name="variable") {
   if ($bool) {
     echo "$name value is TRUE<br>";
@@ -8,6 +12,7 @@ function test($bool,$name="variable") {
 }
 
 function s($i=1) {
+  # helper function that inserts <br> for specified number of times
   while ($i-->0) {
     echo "<br>";
   }
@@ -34,9 +39,78 @@ function untransform($arr) {
   # ["KEY1='VALUE1'", "KEY2='VALUE2'", "KEY3='VALUE3'", "KEY4='VALUE4'"]
   $result = [];
   foreach ($arr as $key => $value) {
-    $result[] = $sprintf("%s='%s'", $key, $value);
+    $result[] = sprintf("%s=%s", $key, $value);
   }
   return $result;
+}
+
+function getColumnInfo($table_name, $database="publications") {
+  # returnvalue['col_count'] : number of columns
+  # returnvalue['col_names'] : array of all column names
+  global $conn;
+
+  # get number of columns as col_count
+  $column_query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='$database' AND TABLE_NAME='$table_name'";
+  if (!$column_table = $conn->query($column_query)) die("'$column_query' failed :DDD");
+  $col_count = $column_table->num_rows;
+
+  # populate col_names[] with all column names
+  for ($j=0; $j<$col_count; $j++) {
+    $row = $column_table->fetch_array(MYSQLI_ASSOC);
+    $col_names[] = $row['COLUMN_NAME'];
+  }
+
+  $returnvalue['col_count'] = $col_count;
+  $returnvalue['col_names'] = $col_names;
+  return $returnvalue;
+}
+
+function displayTable($result, $table_name, $database="publications") {
+  global $conn;
+  $cols = getColumnInfo("classics")['col_count'];
+  $rows = $result->num_rows;
+  echo "<table>";
+  for ($j=0; $j<$rows; $j++) {
+    $row = $result->fetch_array(MYSQLI_NUM);
+    echo "<tr>";
+    for ($k=0; $k<$cols; $k++) {
+      echo "<td> | " . htmlspecialchars($row[$k]) . "</td>";
+    }
+    echo "</tr>";
+  }
+  echo "</table>";
+}
+
+function nonDuplicateInsert($insertcmd) {
+  global $conn;
+  preg_match('/insert into (\w+) values\((.+)\)/i', $insertcmd, $arr);
+  if (count($arr) != 3) { $errmsg="regex match failure for \$insertcmd"; return $errmsg; }
+  $table = $arr[1];
+  $args_str = $arr[2];
+
+  $values_array = preg_split('/\s*(\s*,\s*)*,+\s*(\s*,\s*)*/i', $args_str);
+  $table_column_info = getColumnInfo($table);
+  $col_count = $table_column_info['col_count'];
+  $col_names = $table_column_info['col_names'];
+  if (count($values_array) != $col_count) { $errmsg="wrong number of values in \$insertcmd"; return $errmsg; }
+
+  for ($i=0; $i<$col_count; $i++) {
+    if ($values_array[$i] != 'NULL') {
+      $values_alist[$col_names[$i]] = trim($values_array[$i]);
+    }
+  }
+
+  $values_alist_stringified = implode(" AND ", untransform($values_alist));
+  $query_sel = "SELECT * FROM $table WHERE $values_alist_stringified";
+  if (!$result_sel = $conn->query($query_sel)) die("'$query_sel' failed :DDD");
+
+  if ($result_sel->num_rows == 0) {
+    if (!$result_ins = $conn->query($insertcmd)) die("'$insert_cmd' failed :DDD");
+    return "entry inserted\n";
+  } else {
+    return "entry already exists\n";
+  }
+  return "nonDuplicateInsert_end\n";
 }
 
 function nonDuplicateUpdate($updatecmd) {
@@ -67,12 +141,7 @@ function nonDuplicateUpdate($updatecmd) {
     foreach ($ttable_row as $key => $value) {
       $ttable_row_to_search[] = $set_arr_transformed[$key] ? $set_arr_transformed[$key] : $ttable_row[$key];
     }
-    # Untransform $ttable_row_to_search
-    # $where_requirements = implode $ttable_row_to_search
-    # $query = "SELECT * from $table where $where_requirements"
-    # $result = execute $query
-    # if ($result->num_rows > 0) {
-    #   execute 
   }
 }
+
 ?>
